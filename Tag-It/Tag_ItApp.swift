b0694e4af63ca887c5d61bcb3515c086f336b520
Balystick <balystick74@icloud.com>
2024-10-09 09:42:29 +0200

    First

---
+++ Tag-It/Tag_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tag_ItApp.swift
+//  Tag-It
+//
+//  Created by Aurélien on 09/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Tag_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
