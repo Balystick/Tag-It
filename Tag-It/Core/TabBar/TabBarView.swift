a7b9c6e864f41bae9b318fb87be7018b03afcbba
Marc <marcdvlpr@gmail.com>
2024-10-21 10:28:30 +0200

    Add TabBarView

---
+++ Tag-It/Core/TabBar/TabBarView.swift
@@ -0,0 +1,63 @@
+//
+//  TabBarView.swift
+//  Tag-It
+//
+//  Created by Apprenant 124 on 21/10/2024.
+//
+
+import SwiftUI
+
+enum Tab: String {
+    case home = "house"
+    case map = "mappin.and.ellipse"
+    case camera = "camera.viewfinder"
+    case favorite = "heart"
+    case profile = "person"
+}
+
+struct TabBarView: View {
+    @State private var selectedTab: Tab = .home
+
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            Text("Home")
+                .tabItem {
+                    Image(systemName: Tab.home.rawValue)
+                    Text("Home")
+                }
+                .tag(Tab.home)
+            
+            Text("Map")
+                .tabItem {
+                    Image(systemName: Tab.map.rawValue)
+                    Text("Map")
+                }
+                .tag(Tab.map)
+            
+           Text("Camera")
+                .tabItem {
+                    Image(systemName: Tab.camera.rawValue)
+                    Text("Camera")
+                }
+                .tag(Tab.camera)
+            
+            Text("Favorite")
+                .tabItem {
+                    Image(systemName: Tab.favorite.rawValue)
+                    Text("Favorite")
+                }
+                .tag(Tab.profile)
+            
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: Tab.profile.rawValue)
+                    Text("Profile")
+                }
+                .tag(Tab.profile)
+        }
+    }
+}
+
+#Preview {
+    TabBarView()
+}
