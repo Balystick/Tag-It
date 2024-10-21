ab592d533090385b77fdc96a173f3249265ad51d
Marc <marcdvlpr@gmail.com>
2024-10-21 15:18:10 +0200

    Add Artwork model

---
+++ Tag-It/Model/Artwork.swift
@@ -0,0 +1,20 @@
+//
+//  Artwork.swift
+//  Tag-It
+//
+//  Created by Apprenant 124 on 21/10/2024.
+//
+
+import Foundation
+
+struct Artwork: Codable, Identifiable {
+    let id: UUID
+    let title: String
+    let image: String
+    let type: String
+    let address: String
+    let city: String
+    let country: String
+    let latitude: Double
+    let longitude: Double
+}
