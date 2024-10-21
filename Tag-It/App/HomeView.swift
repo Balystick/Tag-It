316998b77902c48a74c64eafe0f294712895fd15
Audrey <ahda.audrey92@gmail.com>
2024-10-21 10:40:46 +0200

    Creation HomeView

---
+++ Tag-It/App/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Tag-It
+//
+//  Created by Audrey on 21/10/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
