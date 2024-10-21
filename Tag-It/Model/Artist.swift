23c0a2ab724e2bc4c6437851cc5efc391aa731f4
Marc <marcdvlpr@gmail.com>
2024-10-21 15:19:33 +0200

    Add Artist model

---
+++ Tag-It/Model/Artist.swift
@@ -0,0 +1,13 @@
+//
+//  Artist.swift
+//  Tag-It
+//
+//  Created by Apprenant 124 on 21/10/2024.
+//
+
+import Foundation
+
+class Artist: Codable, Identifiable {
+    let id: UUID
+    let title: String
+}
