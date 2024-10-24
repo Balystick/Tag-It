2a7762ba368b4bbea961f84ccaae73fe792f4960
Balystick <balystick74@icloud.com>
2024-10-24 12:46:48 +0200

    Profile

---
+++ Tag-It/Core/Profile/View/ProfileModifyView.swift
@@ -0,0 +1,215 @@
+
+//
+//  ProfileModifyView.swift
+//  Tag-It
+//
+//  Created by Audrey on 23/10/2024.
+//
+
+import SwiftUI
+
+struct ProfileModifyView: View {
+    
+    @State private var selectedImage: UIImage?
+    @State private var showImagePicker = false
+    
+    let headerFont = Font.custom(FontNameManager.Pixel.bold, size: 35)
+    let regularTextFont = Font.custom(FontNameManager.Pixel.regular, size: 25)
+    
+    //Booleens boutons textfield
+    @State private var isModify = false
+    @State private var isCancel = false
+    @State private var isValidate = false
+    //Booleens alert textield
+    @State private var failedEnterName = false
+    @State private var showingAlert = false
+    @State private var showAlert = false
+    //Clear button x in textfield
+    
+    //VAR pour valeur nom dans bouton annuler
+    @State private var oldName: String = ""
+    @State private var newName: String = ""
+    
+    
+    
+    var body: some View {
+        
+        Circle()
+            .fill(Color.black.opacity(0.1))
+            .frame(width: 150, height: 150)
+            .overlay(
+                Group {
+                    if let image = selectedImage {
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFill()
+                            .clipShape(Circle())
+                    } else {
+                        Text("+")
+                            .foregroundColor(.white)
+                            .font(.system(size: 20, weight: .bold))
+                    }
+                }
+            )
+            .onTapGesture {
+                showImagePicker = true
+            }
+        
+        Text("Kamil Bourouiba")
+            .font(headerFont)
+            .fontWeight(.bold)
+            .foregroundColor(.black)
+        
+        //        VStack {
+        //            // Textfield pour modifier nom profil
+        //            if isModify {
+        //                TextField("Entrer votre nom", text: $viewModel.utilisateur.nom)
+        //                    .font(.caption)
+        //                    .foregroundColor(Color(.customMediumGray))
+        //                    .textFieldStyle(.roundedBorder)
+        //                    .frame(minWidth: 0, maxWidth: 180, minHeight: 0, maxHeight: 30)
+        //                    .textContentType(.username)
+        //                    .onSubmit {
+        //                        isValidate.toggle()
+        //                        viewModel.addUtilisateur()
+        //                        viewModel.saveName(viewModel.utilisateur.nom)
+        //                        isModify = false
+        //                    }
+        //                HStack {
+        //                    Spacer()
+        //                    //Bouton annuler et retour au text
+        //                    Button(action: {
+        //                        viewModel.utilisateur.nom = oldName
+        //                        isCancel = true
+        //                        isModify = false
+        //                    }, label: {
+        //                        Text("Annuler")
+        //                            .font(/*@START_MENU_TOKEN@*/.footnote/*@END_MENU_TOKEN@*/)
+        //                            .fontWeight(/*@START_MENU_TOKEN@*/.thin/*@END_MENU_TOKEN@*/)
+        //                            .animation(.easeOut(duration: 0.2), value: isModify)
+        //                            .foregroundColor(.gray)
+        //                    })
+        //
+        //                    //Bouton valider et enregistrer nom dans view model et userdefaults
+        //                    Button(action: {
+        //                        //Ajout contraintes texte avec alerte si pas respectées
+        //                        //    Aurélien                                    self.saveName()
+        //                        self.failedEnterName = false
+        //                        if (self.viewModel.utilisateur.nom.isEmpty || self.viewModel.utilisateur.nom.count < 3){
+        //                            showingAlert.toggle()
+        //                            self.failedEnterName.toggle()
+        //                        } else {
+        //                            //Fonction pour garder le nom dans le model utilisateur
+        //                            viewModel.addUtilisateur()
+        //                            //Fonction pour garder le nom dans les Userdefaults
+        //                            newName = viewModel.utilisateur.nom
+        //                            viewModel.saveName(newName)
+        //                            oldName = viewModel.utilisateur.nom
+        //                            isValidate.toggle()
+        //                            isModify = false
+        //
+        //                            if oldName != newName{
+        //                                showAlert.toggle()
+        //                                isValidate.toggle()
+        //                                isModify = false
+        //                            }
+        //                        }
+        //                    }, label: {
+        //                        Text("Valider")
+        //                            .font(.footnote)
+        //                            .fontWeight(/*@START_MENU_TOKEN@*/.thin/*@END_MENU_TOKEN@*/)
+        //                            .foregroundColor(Color(.customBlue))
+        //                    })
+        //                    //Affiche alerte si contraintes pas respectées
+        //                    .alert(isPresented: $showingAlert) {
+        //                        if self.failedEnterName {
+        //                            return Alert(title: Text("Le nom doit contenir au moins trois lettres"), message: Text("Veuillez modifier"), dismissButton: .default(Text("OK")))
+        //                        }else {
+        //                            return Alert(title: Text("Validé"), message: Text("Changé"), dismissButton: .default(Text("OK")))
+        //                        }
+        //                    }
+        //                    .font(/*@START_MENU_TOKEN@*/.footnote/*@END_MENU_TOKEN@*/)
+        //                    .fontWeight(/*@START_MENU_TOKEN@*/.thin/*@END_MENU_TOKEN@*/)
+        //                    .scaleEffect(isModify ? 1.2 : 1)
+        //                    .animation(.easeOut(duration: 0.2), value: isModify)
+        //                }
+        //                .padding(.horizontal, 15)
+        //            } else {
+        //                if viewModel.utilisateur.nom.isEmpty{
+        //                    Text("Veuillez entrer votre nom")
+        //                        .foregroundColor(Color(.customMediumGray))
+        //                        .font(.caption)
+        //                } else {
+        //                    //Nom profil affiché
+        //                    Text(viewModel.utilisateur.nom)
+        //                        .multilineTextAlignment(.leading)
+        //                        .frame(minWidth: 0, maxWidth: 180, minHeight: 0, maxHeight: 30)
+        //                        .overlay(
+        //                            RoundedRectangle(cornerRadius: 5)
+        //                                .stroke(.gray, lineWidth: 0.2)
+        //                                .shadow(radius: 20)
+        //                        )
+        //                        .shadow(radius: 10)
+        //                }
+    }
+    
+    // MARK: - Image Picker
+    struct ImagePicker: UIViewControllerRepresentable {
+        @Binding var image: UIImage?
+        
+        class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+            @Binding var image: UIImage?
+            
+            init(image: Binding<UIImage?>) {
+                _image = image
+            }
+            
+            func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+                if let selectedImage = info[.originalImage] as? UIImage {
+                    image = selectedImage
+                }
+                picker.dismiss(animated: true)
+            }
+            
+            func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+                picker.dismiss(animated: true)
+            }
+        }
+        
+        func makeCoordinator() -> Coordinator {
+            return Coordinator(image: $image)
+        }
+        
+        func makeUIViewController(context: Context) -> UIImagePickerController {
+            let picker = UIImagePickerController()
+            picker.delegate = context.coordinator
+            picker.sourceType = .photoLibrary
+            return picker
+        }
+        
+        func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+        
+        
+        
+        public func getCustomFontNames() {
+            // get each of the font families
+            for family in UIFont.familyNames.sorted() {
+                let names = UIFont.fontNames(forFamilyName: family)
+                // print array of names
+                print("Family: \(family) Font names: \(names)")
+            }
+        }
+    }
+}
+    
+public struct FontNameManager {
+    //MARK: name of font family
+    struct Pixel {
+        static let regular = "PixelifySans-Medium"
+        static let bold = "PixelifySans-Bold"
+        // add rest of the font style names
+    }
+}
+#Preview {
+    ProfileModifyView()
+}
--- Tag-It/Core/Profile/View/ProfileView.swift
+++ Tag-It/Core/Profile/View/ProfileView.swift
@@ -1,50 +1,33 @@
 //
-//  ProfileView.swift
+//  UserView.swift
 //  Tag-It
 //
-//  Created by Apprenant 122 on 10/22/24.
+//  Created by Audrey on 23/10/2024.
 //
 
-
+import SwiftUI
 import SwiftUI
 import Combine // Import Combine for using Publishers
 
 struct ProfileView: View {
-    @State private var progress: Float = 0.7
-    @State private var timeRemaining = 5
+
     @State private var selectedImage: UIImage?
     @State private var showImagePicker = false
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    let quests = ["Find 8 french pieces", "Find 2 italian pieces", "Find 7 german pieces"]
-
+    
     var body: some View {
         VStack(spacing: 0) {
             // Profile Header
             ProfileHeader(selectedImage: $selectedImage, showImagePicker: $showImagePicker)
-
-            // Progress Bar
-            ProgressBar(progress: progress)
-
             Spacer()
-            HStack(spacing: 20) {
-                                ForEach(0..<3) { _ in
-                                    Circle()
-                                        .fill(Color.white.opacity(0.15))
-                                        .frame(width: 80, height: 80)
-                                }
-                            }.padding()
             Spacer()
-
-            // Quests and Timer
-            QuestSection(timeRemaining: $timeRemaining, quests: quests, timer: timer)
-
             Spacer()
         }
         .padding(24)
-        .background(Color.gray.ignoresSafeArea())
-        .sheet(isPresented: $showImagePicker) {
-            ImagePicker(image: $selectedImage)
-        }
+        .ignoresSafeArea()
+//        .background(Color.gray.ignoresSafeArea())
+//        .sheet(isPresented: $showImagePicker) {
+//            ImagePicker(image: $selectedImage)
+//        }
     }
 }
 
@@ -52,159 +35,217 @@ struct ProfileView: View {
 struct ProfileHeader: View {
     @Binding var selectedImage: UIImage?
     @Binding var showImagePicker: Bool
-
+    @StateObject private var profileViewModel = ProfileViewModel()
+    
+    let headerFont = Font.custom(FontNameManager.Pixel.bold, size: 25)
+    let regularTextFont = Font.custom(FontNameManager.Pixel.regular, size: 25)
+    
     var body: some View {
-        HStack(spacing: 16) {
-            // Profile Image
-            Circle()
-                .fill(Color.white.opacity(0.1))
-                .frame(width: 90, height: 90)
-                .overlay(
-                    Group {
-                        if let image = selectedImage {
-                            Image(uiImage: image)
-                                .resizable()
-                                .scaledToFill()
-                                .clipShape(Circle())
-                        } else {
-                            Text("PP")
-                                .foregroundColor(.white)
-                                .font(.system(size: 20, weight: .bold))
-                        }
-                    }
-                )
-                .onTapGesture {
-                    showImagePicker = true
+        VStack {
+
+//            Image(systemName: "pencil.circle")
+//                .renderingMode(.original)
+//                .font(.largeTitle)
+//                .padding()
+//                .foregroundStyle(.blue)
+//                .multilineTextAlignment(.trailing)
+        
+//            Circle()
+//                .fill(Color.black.opacity(0.1))
+//                .frame(width: 150, height: 150)
+//                .overlay(
+//                    Group {
+//                        if let image = selectedImage {
+//                            Image(uiImage: image)
+//                                .resizable()
+//                                .scaledToFill()
+//                                .clipShape(Circle())
+//                        } else {
+//                            Text("+")
+//                                .foregroundColor(.white)
+//                                .font(.system(size: 20, weight: .bold))
+//                        }
+//                    }
+//                )
+            let defaultImageProfile = "defaultUserImage.jpg"
+            let imageURL = URL(string: "http://localhost:8080/users/\(profileViewModel.user?.image ?? defaultImageProfile)")
+
+            AsyncImage(url: imageURL) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 150, height: 150)
+                        .clipShape(Circle())
+                } else if phase.error != nil {
+                    Text("Erreur de chargement")
+                        .foregroundColor(.red)
+                } else {
+                    ProgressView()
                 }
+            }
+            .padding(.top, 20)
+//                .onTapGesture {
+//                    showImagePicker = true
+//                }
 
             // Profile Info
-            VStack(alignment: .leading, spacing: 4) {
-                Text("Kamil Bourouiba")
-                    .font(.title3)
+            VStack{
+                Text(profileViewModel.user?.name ?? "User default")
+                    .font(headerFont)
                     .fontWeight(.bold)
-                    .foregroundColor(.white)
+                    .foregroundColor(.black)
+                
+                Image("cat.success")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    
+                VStack{
+                    HStack{
+                        Spacer()
+                        Spacer()
+                        Image("heart.arrow")
+                            .resizable()
+                            .frame(width:50, height: 50)
+                        Spacer()
+                        Text("Oeuvres:")
+                            .font(regularTextFont)
+                            .font(.title2)
+                            .fontWeight(.bold)
+                        Text("23")
+                            .font(regularTextFont)
+                            .font(.title2)
+                            .fontWeight(.bold)
+                        Spacer()
+                        Spacer()
+                    }
+                    HStack{
+                        Spacer()
+                        Spacer()
+                        Image("artist")
+                            .resizable()
+                            .frame(width:50, height: 50)
+                        Spacer()
+                        Text("Artistes:")
+                            .font(regularTextFont)
+                            .font(.title2)
+                            .fontWeight(.bold)
+                        Text("12")
+                            .font(regularTextFont)
+                            .font(.title2)
+                            .fontWeight(.bold)
+                        Spacer()
+                        Spacer()
+                    }
+                    HStack{
+                        Spacer()
+                        Spacer()
+                        Image("medal")
+                            .resizable()
+                            .frame(width:50, height: 50)
+                        Spacer()
+                        Text("Points:")
+                            .font(regularTextFont)
+                            .font(.title2)
+                            .fontWeight(.bold)
+                        Text("\(profileViewModel.user?.points ?? 0)")
+                            .font(regularTextFont)
+                            .font(.title2)
+                            .fontWeight(.bold)
+                        Spacer()
+                        Spacer()
 
-                Text("Level 7")
-                    .font(.subheadline)
-                    .foregroundColor(.white.opacity(0.8))
+                    }
+                    HStack{
+                        Spacer()
+                        Spacer()
+                        Image("winner")
+                            .resizable()
+                            .frame(width:50, height: 50)
+                        Spacer()
+                        Text("Rang:")
+                            .font(regularTextFont)
+                            .font(.title2)
+                            .fontWeight(.bold)
+                        Text("3")
+                            .font(regularTextFont)
+                            .font(.title2)
+                            .fontWeight(.bold)
+                        Spacer()
+                        Spacer()
+                    }
+                }
             }
-
-            Spacer()
-            Spacer()
-            // Badge
-            Text("TOP 1%")
-                .font(.system(size: 12, weight: .bold))
-                .padding(.horizontal, 10)
-                .padding(.vertical, 5)
-                .background(Color.white.opacity(0.15))
-                .foregroundColor(.white)
-                .cornerRadius(12)
         }
-        .padding(.bottom, 20)
+        .padding(20)
+        .onAppear {
+            profileViewModel.fetchUser(by: UUID(uuidString: "3f223c8d-9e67-4fad-8aca-ae563172b205")!)
+        }
     }
 }
 
 // MARK: - Progress Bar
-struct ProgressBar: View {
-    var progress: Float
-
-    var body: some View {
-        GeometryReader { geometry in
-            ZStack(alignment: .leading) {
-                Rectangle()
-                    .fill(Color.white.opacity(0.1))
-                    .frame(height: 12)
-                    .cornerRadius(6)
-
-                Rectangle()
-                    .fill(Color.white.opacity(0.3))
-                    .frame(width: geometry.size.width * CGFloat(progress), height: 12)
-                    .cornerRadius(6)
-            }
-        }
-        .frame(height: 12)
-        .padding(.bottom, 20)
-    }
-}
+//struct ProgressBar: View {
+//    var progress: Float
+//
+//    var body: some View {
+//        GeometryReader { geometry in
+//            ZStack(alignment: .leading) {
+//                Rectangle()
+//                    .fill(Color.white.opacity(0.1))
+//                    .frame(height: 12)
+//                    .cornerRadius(6)
+//
+//                Rectangle()
+//                    .fill(Color.white.opacity(0.3))
+//                    .frame(width: geometry.size.width * CGFloat(progress), height: 12)
+//                    .cornerRadius(6)
+//            }
+//        }
+//        .frame(height: 12)
+//        .padding(.bottom, 20)
+//    }
+//}
 
 // MARK: - Quest Section
-struct QuestSection: View {
-    @Binding var timeRemaining: Int
-    let quests: [String]
-    let timer: Publishers.Autoconnect<Timer.TimerPublisher>
+//struct QuestSection: View {
+//    @Binding var timeRemaining: Int
+//    let quests: [String]
+//    let timer: Publishers.Autoconnect<Timer.TimerPublisher>
+//
+//    var body: some View {
+//        VStack {
+//            if timeRemaining == 0 {
+//                Text("Finish them all!")
+//                    .padding()
+//                    .foregroundColor(.white)
+//                    .font(.system(size: 20, weight: .bold))
+//            } else {
+//                Text("New missions in: \(timeRemaining)s")
+//                    .onReceive(timer) { _ in
+//                        if timeRemaining > 0 {
+//                            timeRemaining -= 1
+//                        }
+//                    }
+//                    .padding()
+//                    .foregroundColor(.white)
+//                    .font(.system(size: 20, weight: .bold))
+//            }
+//
+//            ForEach(quests.indices, id: \.self) { index in
+//                HStack {
+//                    Text(timeRemaining == 0 ? quests[index] : "Quest loading ...")
+//                        .padding()
+//                        .foregroundColor(.white)
+//                        .font(.system(size: 20, weight: .bold))
+//                    Spacer()
+//                }
+//            }
+//        }
+//    }
+//}
 
-    var body: some View {
-        VStack {
-            if timeRemaining == 0 {
-                Text("Finish them all!")
-                    .padding()
-                    .foregroundColor(.white)
-                    .font(.system(size: 20, weight: .bold))
-            } else {
-                Text("New missions in: \(timeRemaining)s")
-                    .onReceive(timer) { _ in
-                        if timeRemaining > 0 {
-                            timeRemaining -= 1
-                        }
-                    }
-                    .padding()
-                    .foregroundColor(.white)
-                    .font(.system(size: 20, weight: .bold))
-            }
 
-            ForEach(quests.indices, id: \.self) { index in
-                HStack {
-                    Text(timeRemaining == 0 ? quests[index] : "Quest loading ...")
-                        .padding()
-                        .foregroundColor(.white)
-                        .font(.system(size: 20, weight: .bold))
-                    Spacer()
-                }
-            }
-        }
-    }
-}
-
-// MARK: - Image Picker
-struct ImagePicker: UIViewControllerRepresentable {
-    @Binding var image: UIImage?
-
-    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-        @Binding var image: UIImage?
-
-        init(image: Binding<UIImage?>) {
-            _image = image
-        }
-
-        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-            if let selectedImage = info[.originalImage] as? UIImage {
-                image = selectedImage
-            }
-            picker.dismiss(animated: true)
-        }
-
-        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-            picker.dismiss(animated: true)
-        }
-    }
-
-    func makeCoordinator() -> Coordinator {
-        return Coordinator(image: $image)
-    }
-
-    func makeUIViewController(context: Context) -> UIImagePickerController {
-        let picker = UIImagePickerController()
-        picker.delegate = context.coordinator
-        picker.sourceType = .photoLibrary
-        return picker
-    }
-
-    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
-}
-
-// Preview
-struct ProfileView_Previews: PreviewProvider {
-    static var previews: some View {
-        ProfileView()
-    }
+#Preview {
+    ProfileView()
 }
